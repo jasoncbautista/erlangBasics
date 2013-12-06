1a15c84252c0824d56e227e5cada945372becb46
Jason B <bjason@gmail.com>
2013-12-06 00:19:47 -0800

    Erlang hello world

---
+++ helloWorld.erl
@@ -0,0 +1,23 @@
+% hello world program
+-module(helloWorld).
+-export([start/0]).
+
+
+
+
+% [ ]Mess with switch statements
+% [ ] Calling other methods
+
+% [ ] Print even numbers
+
+
+
+% [ ] binary tree
+%
+% [ ] trie? 
+
+
+start() ->
+    io:fwrite("wassa\n").
+
+
