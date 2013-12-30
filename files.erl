7f18963ff594de61ddd4f94fafac9ba66f1dfdc3
Jason B <bjason@gmail.com>
2013-12-30 08:51:01 -0800

    made into functions

--- files.erl
+++ files.erl
@@ -3,13 +3,14 @@
 %  Need to read / write files
 %  Read json into record?
 % Write json into record
-%
-%
 
 
 -export([main/0]).
 -export([readlines/1]).
 -export([get_all_lines/1]).
+
+-export([readAllAtonce/1]).
+-export([read/1]).
 
 main() ->
     io:fwrite("hello world"),
@@ -31,5 +32,15 @@ get_all_lines(Device) ->
         Line -> Line ++ get_all_lines(Device)
     end.
 
+% All at once
+readAllAtonce(Filename) ->
+    {ok, Bin} = file:read_file(Filename),
+    Bin.
 
+% Line by line
+read(File) ->
+    case file:read_line(File) of
+        {ok, Data} -> [Data | read(File)];
+        eof        -> []
+    end.
 
