b2bab6e18080e49d07ddf4778ce7a0a8e7fa60a6
Jason <jason.bautista@sqor.com>
2014-01-24 10:31:43 -0800

    Dolphin functions

---
+++ dolphins.erl
@@ -0,0 +1,30 @@
+-module(dolphins).
+-compile(export_all).
+
+
+
+
+dolphin1() ->
+    receive
+        do_a_flip ->
+            io:format("No thanks ~n");
+        fish ->
+            io:format("Taking fish.~n");
+        _ ->
+            io:format("All catch ~n.")
+    end.
+
+
+dolphin2() ->
+    receive
+        {From , do_a_flip}
+            From ! "No thanks ~n";
+        {From, fish}
+            From ! "all fish.~n";
+        _ ->
+            io:format("All catch....~n")
+    end.
+            
+
+
+
