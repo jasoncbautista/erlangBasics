e53b11be01383f10c7c90fff4d46c28360f4646d
Jason <bjason@gmail.com>
2014-01-26 08:46:11 -0800

    More of fridge example.

--- kitchen.erl
+++ kitchen.erl
@@ -6,7 +6,9 @@ fridge1() ->
         {From, {store, Food}} ->
             From ! {self(), ok},
             fridge1();
-
+        {From, {take, _Food}} ->
+            From ! {self(), not_found},
+            fridge1();
         terminate ->
             ok
 end.
